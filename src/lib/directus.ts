00dbd4bf0a736e446bb921f81ffcfc66291155e1
Christoph Kopf <ckopf86@gmail.com>
2026-06-02 14:46:33 +0200

    feat: migrate CMS from Strapi to Directus

---
+++ src/lib/directus.ts
@@ -0,0 +1,33 @@
+import { createDirectus, rest, readItems, readItem } from '@directus/sdk';
+
+interface Stueck {
+  id: string;
+  status: string;
+  titel: string;
+  auffuehrung_im_jahr: number;
+  synopsis: string;
+  flyer?: string;
+  titelfoto?: string;
+  foto_gallerie?: any[];
+}
+
+interface Theatermensch {
+  id: string;
+  status: string;
+  name: string;
+  funktionen: string;
+  steckbrief: string;
+  foto?: string;
+  foto_funny?: string;
+  aktiver_theatermensch: boolean;
+}
+
+interface Schema {
+  stuecke: Stueck[];
+  theatermenschen: Theatermensch[];
+}
+
+const client = createDirectus<Schema>('https://cms.kopfarbeit.dev').with(rest());
+
+export default client;
+export { readItems, readItem };
